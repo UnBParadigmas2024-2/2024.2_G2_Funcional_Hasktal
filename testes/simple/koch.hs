8344c64c096453b8ca36ea51b2d5f91f1b1a07ed
gitbmvb <bruno.martval@gmail.com>
2024-11-06 20:29:27 -0300

    add: adiciona efeito de zoom

--- testes/simple/koch.hs
+++ testes/simple/koch.hs
@@ -10,6 +10,36 @@ iterations = 5
 initialZoom :: Float
 initialZoom = 0.05
 
+main :: IO ()
+main = animate window background (kochSnowflakeZooming iterations)
+
+-- Função que define o floco de neve de Koch
+kochSnowflakeZooming :: Int -> Float -> Picture
+kochSnowflakeZooming maxIterations t =
+  let
+    -- Profundidade atual da recursão, baseada no tempo
+    currentIterations = min maxIterations (floor t)
+    
+    -- Pontos de um triângulo equilátero
+    p1 = (-200, -115)
+    p2 = (200, -115)
+    p3 = (0, 346)
+    
+    -- Gera os três lado do floco de neve
+    path1 = kochFractal currentIterations p1 p2
+    path2 = kochFractal currentIterations p2 p3
+    path3 = kochFractal currentIterations p3 p1
+    
+    -- Adapata o fator de escala baseado na profundidade da recursão
+    zoomFactor = initialZoom + t * 2
+    
+    -- Aplica o fator de zoom para os caminhos do fractal
+    scaledPath1 = scalePath zoomFactor path1
+    scaledPath2 = scalePath zoomFactor path2
+    scaledPath3 = scalePath zoomFactor path3
+  in
+    color white (pictures [line scaledPath1, line scaledPath2, line scaledPath3])
+
 -- Função que gera os pontos de um lado do fractal
 kochFractal :: Int -> Point -> Point -> Path
 kochFractal 0 p1 p2 = [p1, p2]
@@ -29,3 +59,7 @@ kochFractal n (x1, y1) (x2, y2) =
     kochFractal (n - 1) pA pPeak ++
     kochFractal (n - 1) pPeak pB ++
     kochFractal (n - 1) pB (x2, y2)
+
+-- Função para adaptar a escala ao fator de zoom
+scalePath :: Float -> Path -> Path
+scalePath zoomFactor = map (\(x, y) -> (x * zoomFactor, y * zoomFactor))
