45106c744d18463482bc521faaa49fcfbdef226f
Igor Penha <igor.penharol@gmail.com>
2024-11-06 15:38:27 -0300

    Solução recursiva para o fractal de triangulo de sierpinski

--- triangulo-sierpinski/src/Lib.hs
+++ triangulo-sierpinski/src/Lib.hs
@@ -1,6 +1,14 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (sierpinski, drawTriangle) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type MyPoint = (Float, Float)
+type Triangle = (MyPoint, MyPoint, MyPoint)
+
+sierpinski :: Int -> Triangle -> [Triangle]
+sierpinski 0 tri = [tri]
+sierpinski n (p1, p2, p3) =
+    sierpinski (n - 1) (p1, midPoint p1 p2, midPoint p1 p3) ++
+    sierpinski (n - 1) (p2, midPoint p2 p3, midPoint p1 p2) ++
+    sierpinski (n - 1) (p3, midPoint p1 p3, midPoint p2 p3)
+  where
+    midPoint (x1, y1) (x2, y2) = ((x1 + x2) / 2, (y1 + y2) / 2)
+
