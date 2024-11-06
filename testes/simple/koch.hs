ee1edd8d6bb37231213e908b5f19c1387803bd4d
gitbmvb <bruno.martval@gmail.com>
2024-11-06 20:22:40 -0300

    add: adiciona função que calcula o fractal em si

--- testes/simple/koch.hs
+++ testes/simple/koch.hs
@@ -10,3 +10,22 @@ iterations = 5
 initialZoom :: Float
 initialZoom = 0.05
 
+-- Função que gera os pontos de um lado do fractal
+kochFractal :: Int -> Point -> Point -> Path
+kochFractal 0 p1 p2 = [p1, p2]
+kochFractal n (x1, y1) (x2, y2) =
+  let
+    -- Pontos necessários para o "bump" do fractal
+    dx = (x2 - x1) / 3
+    dy = (y2 - y1) / 3
+    pA = (x1 + dx, y1 + dy)
+    pB = (x2 - dx, y2 - dy)
+    angle = pi / 3
+    pPeak = (x1 + dx + cos angle * dx - sin angle * dy,
+             y1 + dy + sin angle * dx + cos angle * dy)
+  in
+    -- Gera recursivamente os pontos para cada segmento
+    kochFractal (n - 1) (x1, y1) pA ++
+    kochFractal (n - 1) pA pPeak ++
+    kochFractal (n - 1) pPeak pB ++
+    kochFractal (n - 1) pB (x2, y2)
